5af58fbbb5ac63a5dbddb801efa64d61c924afb1
Vennela <S544925@nwmissouri.edu>
2022-05-31 10:51:12 -0500

    Angular Demo Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,13 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     
   }
-  title = 'AngularDemo';
   result!:number;
   Add(num1:string, num2:string)
   {
     this.result=parseFloat(num1)+parseFloat(num2);
+  }
+  Sub(num1:string, num2:string)
+  {
+    this.result=parseFloat(num1)-parseFloat(num2);
   }
 }
